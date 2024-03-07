3485f1dfd0d820872b5a9b24f7ab48715c68a91f
firlygrsld <144504913+firlygrsld@users.noreply.github.com>
2024-03-07 09:28:48 +0700

    Update Tugas

--- pacepal/lib/forgotpassword.dart
+++ pacepal/lib/forgotpassword.dart
@@ -1,15 +1,113 @@
 import 'package:flutter/material.dart';
+import 'package:pacepal/loginscreen.dart';
 
 class ForgotPassword extends StatefulWidget {
-  const ForgotPassword({super.key});
+  const ForgotPassword({Key? key}) : super(key: key);
 
   @override
   State<ForgotPassword> createState() => _ForgotPasswordState();
 }
 
 class _ForgotPasswordState extends State<ForgotPassword> {
+  bool isCheckedRememberMe = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/img/pacepal.jpg',
+              height: 150,
+              width: 150,
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.all(5),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'email',
+                      border: UnderlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'new password',
+                      border: UnderlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'confirm password',
+                      border: UnderlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        height: 50,
+                        width: 50,
+                        child: Theme(
+                          data: ThemeData(
+                              unselectedWidgetColor: Colors.blue[900]),
+                          child: Checkbox(
+                            activeColor: Colors.blue[900],
+                            value: isCheckedRememberMe,
+                            onChanged: (value) {
+                              setState(() {
+                                isCheckedRememberMe = value!;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 20),
+                      Text(
+                        'remember me',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10),
+                  ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => LoginScreen(),
+                        ),
+                      );
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      primary: const Color.fromARGB(255, 255, 149, 0),
+                    ),
+                    child: Text(
+                      'SET',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 40),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
