c7d82d4b185976c64520f1461fe105820c3e18c1
firlygrsld <144504913+firlygrsld@users.noreply.github.com>
2024-05-02 22:18:39 +0700

    Update Tugas

--- pacepal/lib/loginscreen.dart
+++ pacepal/lib/loginscreen.dart
@@ -13,164 +13,177 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   bool isCheckedRememberMe = false;
+  bool _obscureText = true;
+
+  void _togglePasswordVisibility() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.white,
-        body: Column(
-          children: [
-            Image.asset(
-              'assets/img/pacepal.jpg',
-              height: 170,
-              width: 150,
-            ),
-            SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 100, top: 30, right: 100, bottom: 10),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'username',
-                      border: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          Image.asset(
+            'assets/img/pacepal.jpg',
+            height: 170,
+            width: 150,
+          ),
+          SizedBox(height: 10),
+          Padding(
+            padding: const EdgeInsets.only(
+                left: 100, top: 30, right: 100, bottom: 10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'username',
+                    border: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextFormField(
+                  obscureText: _obscureText,
+                  decoration: InputDecoration(
+                    labelText: 'password',
+                    border: UnderlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black),
+                    ),
+                    suffixIcon: IconButton(
+                      onPressed: _togglePasswordVisibility,
+                      icon: Icon(
+                        _obscureText ? Icons.visibility_off : Icons.visibility,
                       ),
                     ),
                   ),
-                  SizedBox(height: 10),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'password',
-                      border: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black),
-                      ),
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        height: 50,
-                        width: 50,
-                        child: Theme(
-                          data: ThemeData(
-                              unselectedWidgetColor:
-                                  Color.fromARGB(255, 2, 30, 71)),
-                          child: Checkbox(
-                            activeColor: Color.fromARGB(255, 2, 30, 71),
-                            value: isCheckedRememberMe,
-                            onChanged: (value) {
-                              setState(() {
-                                isCheckedRememberMe = value!;
-                              });
-                            },
-                          ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      height: 50,
+                      width: 50,
+                      child: Theme(
+                        data: ThemeData(
+                            unselectedWidgetColor:
+                                Color.fromARGB(255, 2, 30, 71)),
+                        child: Checkbox(
+                          activeColor: Color.fromARGB(255, 2, 30, 71),
+                          value: isCheckedRememberMe,
+                          onChanged: (value) {
+                            setState(() {
+                              isCheckedRememberMe = value!;
+                            });
+                          },
                         ),
                       ),
-                      SizedBox(height: 20),
-                      Text(
-                        'remember me',
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => HomeScreen()));
-                    },
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      backgroundColor: const Color.fromARGB(255, 255, 149, 0),
                     ),
-                    child: Text(
-                      'LOGIN',
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold),
+                    SizedBox(height: 20),
+                    Text(
+                      'remember me',
+                      style: TextStyle(color: Colors.grey),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushReplacement(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ForgotPassword()));
-                    },
-                    child: Text(
-                      'forgot password?',
-                      style: TextStyle(
-                        color: Colors.grey,
-                      ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) => HomeScreen()));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
                     ),
+                    backgroundColor: const Color.fromARGB(255, 255, 149, 0),
                   ),
-                  SizedBox(height: 40),
-                  Text(
-                    'Or login with',
+                  child: Text(
+                    'LOGIN',
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(height: 10),
+                GestureDetector(
+                  onTap: () {
+                    Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ForgotPassword()));
+                  },
+                  child: Text(
+                    'forgot password?',
                     style: TextStyle(
                       color: Colors.grey,
                     ),
                   ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      IconButton(
-                        icon: FaIcon(FontAwesomeIcons.google),
-                        onPressed: () {},
-                      ),
-                      SizedBox(width: 10),
-                      IconButton(
-                        icon: FaIcon(FontAwesomeIcons.facebook),
-                        onPressed: () {},
-                      ),
-                      SizedBox(width: 10),
-                      IconButton(
-                        icon: FaIcon(FontAwesomeIcons.twitter),
-                        onPressed: () {},
-                      ),
-                      SizedBox(width: 20),
-                    ],
+                ),
+                SizedBox(height: 40),
+                Text(
+                  'Or login with',
+                  style: TextStyle(
+                    color: Colors.grey,
                   ),
-                  SizedBox(height: 10),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        "Don't have an account? ",
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: FaIcon(FontAwesomeIcons.google),
+                      onPressed: () {},
+                    ),
+                    SizedBox(width: 10),
+                    IconButton(
+                      icon: FaIcon(FontAwesomeIcons.facebook),
+                      onPressed: () {},
+                    ),
+                    SizedBox(width: 10),
+                    IconButton(
+                      icon: FaIcon(FontAwesomeIcons.twitter),
+                      onPressed: () {},
+                    ),
+                    SizedBox(width: 20),
+                  ],
+                ),
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't have an account? ",
+                      style: TextStyle(
+                        color: Color.fromARGB(255, 2, 30, 71),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SignUpScreen()),
+                        );
+                      },
+                      child: Text(
+                        "Sign Up",
                         style: TextStyle(
-                          color: Color.fromARGB(255, 2, 30, 71),
+                          color: Color.fromARGB(255, 255, 149, 0),
                         ),
                       ),
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => SignUpScreen()),
-                          );
-                        },
-                        child: Text(
-                          "Sign Up",
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 255, 149, 0),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                    ),
+                  ],
+                ),
+              ],
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
   }
 }
--- pacepal/lib/signupscreen.dart
+++ pacepal/lib/signupscreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:pacepal/homescreen.dart';
 
 class SignUpScreen extends StatefulWidget {
@@ -10,119 +12,228 @@ class SignUpScreen extends StatefulWidget {
 
 class _SignUpScreenState extends State<SignUpScreen> {
   bool isCheckedRememberMe = false;
+  bool _obscureText = true;
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _phoneNumberController = TextEditingController();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
+
+  void _togglePasswordVisibility() {
+    setState(() {
+      _obscureText = !_obscureText;
+    });
+  }
+
+  void _showSnackBar(String message) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+        backgroundColor: Colors.orange,
+      ),
+    );
+  }
+
+  Future<void> _signup() async {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+
+    String email = _emailController.text.trim();
+    String phoneNumber = _phoneNumberController.text.trim();
+    String username = _usernameController.text.trim();
+    String password = _passwordController.text.trim();
+    String confirmPassword = _confirmPasswordController.text.trim();
+
+    if (email.isEmpty ||
+        phoneNumber.isEmpty ||
+        username.isEmpty ||
+        password.isEmpty ||
+        confirmPassword.isEmpty) {
+      _showSnackBar('Password Mismatch');
+      return;
+    }
+
+    if (password != confirmPassword) {
+      _showSnackBar('Password and confirm password do not match.');
+      return;
+    }
+
+    final url = Uri.parse(
+        'https://pacepal-7f5de-default-rtdb.asia-southeast1.firebasedatabase.app/user.json');
+
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode({
+          'email': email,
+          'phoneNumber': phoneNumber,
+          'username': username,
+          'password': password,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomeScreen(),
+          ),
+        );
+      } else {
+        throw Exception('Failed to add user');
+      }
+    } catch (error) {
+      print("Failed to add user: $error");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Image.asset(
-              'assets/img/pacepal.jpg',
-              height: 150,
-              width: 150,
-            ),
-            SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.only(
-                  left: 100, top: 30, right: 100, bottom: 10),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'email',
-                      border: UnderlineInputBorder(),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Image.asset(
+                'assets/img/pacepal.jpg',
+                height: 150,
+                width: 150,
+              ),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.only(
+                    left: 100, top: 30, right: 100, bottom: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextFormField(
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                        labelText: 'email',
+                        border: UnderlineInputBorder(),
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'phone number',
-                      border: UnderlineInputBorder(),
+                    SizedBox(height: 10),
+                    TextFormField(
+                      controller: _phoneNumberController,
+                      decoration: InputDecoration(
+                        labelText: 'phone number',
+                        border: UnderlineInputBorder(),
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'username',
-                      border: UnderlineInputBorder(),
+                    SizedBox(height: 10),
+                    TextFormField(
+                      controller: _usernameController,
+                      decoration: InputDecoration(
+                        labelText: 'username',
+                        border: UnderlineInputBorder(),
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'password',
-                      border: UnderlineInputBorder(),
-                    ),
-                    obscureText: true,
-                  ),
-                  SizedBox(height: 10),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: 'confirm password',
-                      border: UnderlineInputBorder(),
-                    ),
-                    obscureText: true,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      SizedBox(
-                        height: 50,
-                        width: 50,
-                        child: Theme(
-                          data: ThemeData(
-                              unselectedWidgetColor:
-                                  Color.fromARGB(255, 2, 30, 71)),
-                          child: Checkbox(
-                            activeColor: Color.fromARGB(255, 2, 30, 71),
-                            value: isCheckedRememberMe,
-                            onChanged: (value) {
-                              setState(() {
-                                isCheckedRememberMe = value!;
-                              });
-                            },
+                    SizedBox(height: 10),
+                    TextFormField(
+                      controller: _passwordController,
+                      decoration: InputDecoration(
+                        labelText: 'password',
+                        border: UnderlineInputBorder(),
+                        suffixIcon: IconButton(
+                          onPressed: _togglePasswordVisibility,
+                          icon: Icon(
+                            _obscureText
+                                ? Icons.visibility_off
+                                : Icons.visibility,
                           ),
                         ),
                       ),
-                      SizedBox(height: 20),
-                      Text(
-                        'remember me',
-                        style: TextStyle(color: Colors.grey),
+                      obscureText: _obscureText,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Password must be filled';
+                        }
+                        String pattern =
+                            r'^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[!@#$%^&*()_+{}|<>?]).{8,}$';
+                        RegExp regex = RegExp(pattern);
+                        if (!regex.hasMatch(value)) {
+                          return 'Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 10),
+                    TextFormField(
+                      controller: _confirmPasswordController,
+                      decoration: InputDecoration(
+                        labelText: 'confirm password',
+                        border: UnderlineInputBorder(),
                       ),
-                    ],
-                  ),
-                  SizedBox(height: 10),
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => HomeScreen(),
+                      obscureText: true,
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Confirm Password is required';
+                        }
+                        if (value != _passwordController.text) {
+                          return 'Password do not match!';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 10),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 50,
+                          width: 50,
+                          child: Theme(
+                            data: ThemeData(
+                                unselectedWidgetColor:
+                                    Color.fromARGB(255, 2, 30, 71)),
+                            child: Checkbox(
+                              activeColor: Color.fromARGB(255, 2, 30, 71),
+                              value: isCheckedRememberMe,
+                              onChanged: (value) {
+                                setState(() {
+                                  isCheckedRememberMe = value!;
+                                });
+                              },
+                            ),
+                          ),
                         ),
-                      );
-                    },
-                    style: ElevatedButton.styleFrom(
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      backgroundColor: const Color.fromARGB(255, 255, 149, 0),
+                        SizedBox(height: 20),
+                        Text(
+                          'remember me',
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
                     ),
-                    child: Text(
-                      'SIGN UP',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: _signup,
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        backgroundColor: const Color.fromARGB(255, 255, 149, 0),
+                      ),
+                      child: Text(
+                        'SIGN UP',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 40),
-                ],
+                    SizedBox(height: 40),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
